379065bc1f11a1fdf955de0d286a5269ecc2dac5
zhangdongdong1 <zhangdongdong1@kingsoft.com>
2017-01-09 17:31:11 +0800

    update test help

--- test/api/constant.go
+++ test/api/constant.go
@@ -8,15 +8,15 @@ const (
 	Common      = "\n*************************************************************************************************%s\n"
 )
 
-var accessKey = flag.String("accessKey", "1GL02rRYQxK8s7FQh8dV", "accessKey")
-var secretKey = flag.String("secretKey", "2IDjaPOpFfkq5Zf9K4tKu8k5AKApY8S8eKV1zsRl", "secretKey")
-var Types = flag.String("type", "service", "service")
+var accessKey = flag.String("accKey", "1GL02rRYQxK8s7FQh8dV", "accessKey")
+var secretKey = flag.String("secKey", "2IDjaPOpFfkq5Zf9K4tKu8k5AKApY8S8eKV1zsRl", "secretKey")
+var Types = flag.String("typ", "service", "service")
 var Method = flag.String("method", "GET", "GET,POST,PUT,HEAD,DELETE,OPTIONS")
 var addr = flag.String("addr", "127.0.0.1:20808", "addr")
 
-var content = flag.String("content", "Stand on the shoulders of giants", "content")
-var curfile = flag.String("curfile", "defaultcurrent", "currentfile name")
+var content = flag.String("ctx", "Stand on the shoulders of giants", "PUT method content")
+var curfile = flag.String("curf", "curfile", "current file name")
 
-var bucketName = flag.String("bucketName", "bucketdefault", "bucketName")
+var bucketName = flag.String("bktName", "bk5buckets", "bucket Name")
 var key = flag.String("key", "keydefault", "key")
-var newName = flag.String("newName", "newNamedefault", "newName")
+var newName = flag.String("newName", "newName", "newfile Name")
