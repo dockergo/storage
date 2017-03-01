c7d632512818c28d0094e9c1efdd461e294923b6
flyaways <meagleglass@gmail.com>
2017-03-01 14:03:18 +0800

    update

--- README.md
+++ README.md
@@ -4,28 +4,59 @@
 
 <font color="Green">Supported: s3 siwft posix oss qiniu</font>
 
+## **local run**
+
 ## __<font color="Black">1. Build workspace</font>__
 
 ```sh
- $go get github.com/flyaways/storage
+ $ go get github.com/flyaways/storage
 ```
 
 ## __<font color="Black">2. Build</font>__
 
 ```sh
- $cd storage
- $make
+ $ cd storage
+ $ make
 ```
 
 ## __<font color="Black">3. Run</font>__
 
 ```sh
- $./bin/agent -config=./etc/storage.toml
+ $ ./bin/agent -config=./etc/storage.toml
 ```
 
-## __<font color="Black">4. Testing</font>__
+## **docker run**
 
-**<font color=Green>Parameters List</font>**
+## __<font color="Black">1. Build workspace</font>__
+
+```sh
+ $ go get github.com/flyaways/storage
+```
+
+## __<font color="Black">2. Build</font>__
+
+```sh
+ $ cd storage
+ $ ./build.sh
+```
+
+## __<font color="Black">3. Run</font>__
+
+```sh
+ $ docker run -d --restart=always \
+ $       -p 8080:8080 \
+ $       -e STORAGE_TYPE=s3 \
+ $       -e HTTP_SCHEME=http \
+ $       -e S3_ADDR=192.168.20.4 \
+ $       -e S3_ADDR_PORT=8888 \
+ $       -e S3_ACCESSKEY=ZAZW0PO781UDXLA4HGC7 \
+ $       -e S3_SECRETKEY=ORBLBg0P6kdObZ6uudMDEWuiTUPNKMwArNyHWRNu \
+ $       agent:latest
+```
+
+## **Interface**
+
+**<font color=Green>Method Lists</font>**
 
 |Type|PUT|POST|GET|HEAD|DELETE|
 |---|---|---|---|---|---|
@@ -33,7 +64,7 @@
 |Object|✔|✔|✔|✔|✔||
 |Service|✔|✖|✖|✖|✖||
 
-## __<font color="Black">5. References</font>__
+## **References**
 
 * [__Swift__](http://developer.openstack.org/api-ref/object-storage/)
 * [__s3__](http://docs.s3.com/docs/master/)
--- go/init.sh
+++ go/init.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 
-conf=/go/storage.toml
+conf=/flyaways/storage.toml
 
-sed -i "s,AUTH_ACCESSKEY,$AUTH_ACCESSKEY,g" $conf
-sed -i "s,AUTH_SECRETKEY,$AUTH_SECRETKEY,g" $conf
+# default value
+if [ ! -n "$HTTP_ADDR" ]; then
+sed -i "s,HTTP_ADDR,:8080,g" $agentconf
+else
+sed -i "s,HTTP_ADDR,:$HTTP_ADDR,g" $agentconf
+fi
 
-sed -i "s,HTTP_ADDR,:$HTTP_ADDR,g" $conf
+if [ ! -n "$HTTP_SCHEME" ]; then
+sed -i "s,HTTP_SCHEME,http,g" $agentconf
+else
 sed -i "s,HTTP_SCHEME,$HTTP_SCHEME,g" $conf
+fi
 
+if [ ! -n "$AUTH_ACCESSKEY" ]; then
+sed -i "s,AUTH_ACCESSKEY,ZAZW0PO781UDXLA4HGC7,g" $agentconf
+else
+sed -i "s,AUTH_ACCESSKEY,:$AUTH_ACCESSKEY,g" $agentconf
+fi
+
+if [ ! -n "$AUTH_SECRETKEY" ]; then
+sed -i "s,AUTH_SECRETKEY,ORBLBg0P6kdObZ6uudMDEWuiTUPNKMwArNyHWRNu,g" $agentconf
+else
+sed -i "s,AUTH_SECRETKEY,$AUTH_SECRETKEY,g" $conf
+fi
+
+if [ ! -n "$LOGS_ACCESS" ]; then
+sed -i "s,LOGS_ACCESS,false,g" $conf
+else
 sed -i "s,LOGS_ACCESS,$LOGS_ACCESS,g" $conf
-sed -i "s,LOGS_PATH,$LOGS_PATH,g" $conf
-sed -i "s,LOGS_LEVEL,$LOGS_LEVEL,g" $conf
+fi
 
+if [ ! -n "$LOGS_PATH" ]; then
+sed -i "s,LOGS_PATH,,g" $conf
+else
+sed -i "s,LOGS_PATH,$LOGS_PATH,g" $conf
+fi
+
+if [ ! -n "$LOGS_LEVEL" ]; then
+sed -i "s,LOGS_LEVEL,trace,g" $conf
+else
+sed -i "s,LOGS_LEVEL,$LOGS_LEVEL,g" $conf
+fi
+
+# type must not empty.
 sed -i "s,STORAGE_TYPE,$STORAGE_TYPE,g" $conf
 
 sed -i "s,S3_ACCESSKEY,$S3_ACCESSKEY,g" $conf
@@ -33,5 +67,5 @@ sed -i "s,SWIFT_USERNAME,$SWIFT_USERNAME,g" $conf
 sed -i "s,SWIFT_PROJNAME,$SWIFT_PROJNAME,g" $conf
 sed -i "s,SWIFT_PASSWORD,$SWIFT_PASSWORD,g" $conf
 
-touch /go/init.lock
+touch /flyaways/init.lock
 
